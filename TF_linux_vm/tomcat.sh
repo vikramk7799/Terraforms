8683d2e7381b431407376840c32dce529282648c
Vikram K <vikram.prodevans@gmail.com>
2018-11-30 18:58:32 +0530

    updated path

--- TF_linux_vm/tomcat.sh
+++ TF_linux_vm/tomcat.sh
@@ -2,7 +2,7 @@
 
 sudo yum -y install epel-release phpmyadmin rpm-build redhat-rpm-config;
 sudo yum -y install java tomcat tomcat-webapps tomcat-admin-webapps;
-sudo chmod 777 -R /opt/tomcat;
-sudo cp /tmp/DemoDec.war /opt/tomcat/webapps;
+sudo chmod 777 -R /usr/share/tomcat/webapps;
+sudo cp /tmp/DemoDec.war /usr/share/tomcat/webapps;
 sudo systemctl start tomcat;
 sudo systemctl enable tomcat;
