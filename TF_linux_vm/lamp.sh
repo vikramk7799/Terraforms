73270d85e32e6f6d365e6609bc6414dde4eb43cf
Vikram K <vikram.prodevans@gmail.com>
2018-11-30 18:44:59 +0530

    tomcat

--- TF_linux_vm/lamp.sh
+++ TF_linux_vm/lamp.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
 sudo yum -y install epel-release phpmyadmin rpm-build redhat-rpm-config;
-wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm;
-sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm;
-sudo yum -y install httpd mysql-server mysql-devel php php-mysql php-fpm;
+sudo yum -y install java tomcat tomcat-webapps tomcat-admin-webapps;
+sudo chmod 777 -R /opt/tomcat;
+sudo cp /tmp/DemoDec.war /opt/tomcat/webapps;
+sudo systemctl start tomcat;
+sudo systemctl enable tomcat;
+
+
+
+
+
+
+
+#sudo yum -y install epel-release phpmyadmin rpm-build redhat-rpm-config;
+#wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm;
+#sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm;
+#sudo yum -y install httpd mysql-server mysql-devel php php-mysql php-fpm;
 #sudo yum -y install tinyxml libzip mysql-workbench-community;
-sudo chmod 777 -R /var/www/;
-sudo printf "<?php\nphpinfo();\n?>" > /var/www/html/info.php;
-sudo service mysqld restart;
-sudo service httpd restart;
-sudo chkconfig httpd on;
-sudo chkconfig mysqld on;
+#sudo chmod 777 -R /var/www/;
+#sudo printf "<?php\nphpinfo();\n?>" > /var/www/html/info.php;
+#sudo service mysqld restart;
+#sudo service httpd restart;
+#sudo chkconfig httpd on;
+#sudo chkconfig mysqld on;
